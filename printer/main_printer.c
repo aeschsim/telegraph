ec992cd1a66b0e14f0f0df13c1eb69e82cf2a4e6
aeschsim <aeschsim@students.zhaw.ch>
2017-04-15 12:20:37 +0200

    Add files via upload

--- printer/main_printer.c
+++ printer/main_printer.c
@@ -7,26 +7,56 @@
 #define RELAIS_1	15
 #define RELAIS_2	16
 
-int main(void) 
-{
+int main(void) {
 	wiringPiSetup();
 	pinMode(RELAIS_1, OUTPUT);
 	pinMode(RELAIS_2, OUTPUT);
 	digitalWrite(RELAIS_1, LOW);
 	digitalWrite(RELAIS_2, LOW);
+	delay(1000);
+	digitalWrite(RELAIS_1, HIGH);
+	digitalWrite(RELAIS_2, HIGH);
+	delay(1000);
+	digitalWrite(RELAIS_1, LOW);
+	digitalWrite(RELAIS_2, LOW);	
 	
-	printf("Setup done...")
+	printf("Setup done...");
 		
 	FILE * pFile;
   	char c;
   	
-  	pFile=fopen ("morsecode.txt","r");
+  	pFile = fopen ("morsecode.txt","r");
   	
   	if (pFile==NULL) {
 		perror ("Error opening file");
 		return 1;
 	}
-  	
+	
+	do
+   {
+      c = fgetc(pFile);
+      if( feof(pFile) )
+      {
+         break ;
+      }
+      switch(c) {
+  			case '.':
+  				write_short();
+  				break;
+  			case '-':
+  				write_long();
+  				break;
+  			case ' ':
+  				move();
+  				break;
+		  }
+
+	move();
+	
+   }while(1);
+
+	
+  	/*
   	while(c != EOF) {
   		c = fgetc(pFile);
   		
@@ -44,8 +74,8 @@ int main(void)
 		
 		move();
 	  }
-	
+	*/
 	write_end();
     fclose (pFile);
     return 0;
-}
+	}
--- printer/printer.c
+++ printer/printer.c
@@ -1,44 +1,50 @@
 #include <stdio.h>
 #include <string.h>
+#include "wiringPi.h"
 
 #include "printer.h"
-#include "telegraph.h"
+
+#define RELAIS_1	15
+#define RELAIS_2	16
 
 
 //Zum testen mit printf, später durch /*Code*/ ersetzen!
 void write_short(void)
-{
-	//printf(".");
-	pencil_down();
-	do_step(1);
-	pencil_up();
+{	
+	digitalWrite(RELAIS_1, HIGH);
+	digitalWrite(RELAIS_2, HIGH);
+	delay(1000*1);
+	digitalWrite(RELAIS_2, LOW);
+	digitalWrite(RELAIS_1, LOW);
 }
 
 
 
 void write_long(void)
 {
-	//printf("-");
-
-	pencil_down();
-	do_step(4);
-	pencil_up();
+	digitalWrite(RELAIS_1, HIGH);
+	digitalWrite(RELAIS_2, HIGH);
+	delay(1000*4);
+	digitalWrite(RELAIS_2, LOW);
+	digitalWrite(RELAIS_1, LOW);
 }
 
 
 
 void write_end(void)
 {
-	//printf("XXX");
-	pencil_down();
-	do_step(8);
-	pencil_up();
+	digitalWrite(RELAIS_1, HIGH);
+	digitalWrite(RELAIS_2, HIGH);
+	delay(1000*8);
+	digitalWrite(RELAIS_2, LOW);
+	digitalWrite(RELAIS_1, LOW);
 }
 
 
 
 void move(void) 
 {
-	printf(" ");
-	do_step(3);
+	digitalWrite(RELAIS_2, HIGH);
+	delay(1000*3);
+	digitalWrite(RELAIS_2, LOW);
 }
